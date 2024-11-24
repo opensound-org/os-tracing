38fb981955dbc44709b5d69e3b5ae211b4821154
29 <791603901@qq.com>
2024-11-24 15:51:41 +0800

    ⚗️ Add experimental code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use est::AnyRes;
+use surrealdb::engine::local::RocksDb;
+use surrealdb::Surreal;
+
+#[tokio::main]
+async fn main() -> AnyRes {
+    println!(
+        "v{}",
+        Surreal::new::<RocksDb>(".rocksdb").await?.version().await?
+    );
+    Ok(())
 }
