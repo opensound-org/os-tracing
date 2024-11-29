a285e7a4edea3d788b2e2734ad02441240ff7c2b
29 <791603901@qq.com>
2024-11-29 17:35:08 +0800

    🔥 Remove experimental code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use est::AnyRes;
-use surrealdb::engine::local::RocksDb;
-use surrealdb::Surreal;
-
-#[tokio::main]
-async fn main() -> AnyRes {
-    println!(
-        "v{}",
-        Surreal::new::<RocksDb>(".rocksdb").await?.version().await?
-    );
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
