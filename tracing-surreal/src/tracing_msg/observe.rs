32f48b66cb17c39220ac22d4ebf16305f5e4d9be
29 <791603901@qq.com>
2025-01-11 23:51:33 +0800

    ⚗️ (tracing-msg): 添加`observe::{CloseInfo, MsgInfo}`

--- tracing-surreal/src/tracing_msg/observe.rs
+++ tracing-surreal/src/tracing_msg/observe.rs
@@ -1,4 +1,4 @@
-use super::{Handshake, Role};
+use super::{CloseMsg, Handshake, Role, TracingMsg};
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
@@ -9,4 +9,17 @@ pub struct ClientInfo {
     pub handshake_info: Handshake,
     pub client_role: Role,
     pub client_addr: Option<SocketAddr>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct CloseInfo {
+    pub close_timestamp: DateTime<Local>,
+    pub client_info: ClientInfo,
+    pub close_msg: CloseMsg,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct MsgInfo {
+    pub client_info: ClientInfo,
+    pub tracing_msg: TracingMsg,
 }
